0abc06dea6bf900fa6389b76886b2e1d26207c8c
noahzark <noahzark@vip.qq.com>
2014-02-06 16:26:14 +0800

    Update StringScanner class
    Add the method "findAllString" to get all lines which contains the key
    string in a file.

--- src/xxgamehelper/framework/model/StringScanner.java
+++ src/xxgamehelper/framework/model/StringScanner.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /***
  * A scanner to check a string in a file or get a string in special pattern.
@@ -25,10 +26,21 @@ public class StringScanner {
 	}
 	
 	/***
+	 * Get all lines which contains the key string in a file(Default use UTF-8 char set to open it).
+	 * @param key The key string
+	 * @param fileName The name of target file
+	 * @param charsetName  The char set used to open the file
+	 * @return A string array
+	 */
+	public static String[] findAllString(String key, String fileName) {
+		return StringScanner.findAllString(key, fileName, "UTF-8");
+	}
+	
+	/***
 	 * Check if a key string is in a file.
 	 * @param key The key String
 	 * @param fileName The name of target file
-	 * @param charsetName The charset used to open the file
+	 * @param charsetName The char set used to open the file
 	 * @return Null if the key is not found,
 	 * else return the line where the string first occurred
 	 */
@@ -51,6 +63,36 @@ public class StringScanner {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	/***
+	 * Get all lines which contains the key string in a file.
+	 * @param key The key string
+	 * @param fileName The name of target file
+	 * @param charsetName  The char set used to open the file
+	 * @return A string array
+	 */
+	public static String[] findAllString(String key, String fileName, String charsetName) {
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			InputStreamReader isr = new InputStreamReader(
+					new FileInputStream(fileName), charsetName);
+			BufferedReader br = new BufferedReader(isr);
+			String s;
+			while ((s = br.readLine())!=null){
+				if (s.contains(key)){
+					list.add(s);
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String[] strList = null;
+		strList = list.toArray(strList);
+		return strList;
 	}
 	
 	/***
@@ -77,5 +119,20 @@ public class StringScanner {
 			sb.append(s.charAt(i));
 		}
 		return sb.toString();
+	}
+	
+	/***
+	 * Count the numbers of key contained in source string.
+	 * @param key The key to search
+	 * @param source The source string
+	 * @return The number
+	 */
+	public static int countString(String key, String source) {
+		int n = 0;
+		while (source.contains(key)) {
+			source = source.replaceFirst(key, "*");
+			n++;
+		}
+		return n;
 	}
 }
