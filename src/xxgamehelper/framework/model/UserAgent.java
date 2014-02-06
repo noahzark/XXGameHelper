9ae0f2763928052869b0f54be9cda9c73f8772a8
noahzark <noahzark@vip.qq.com>
2014-02-06 11:51:19 +0800

    Add the User Agent class
    This class contains several static user agent strings.

---
+++ src/xxgamehelper/framework/model/UserAgent.java
@@ -0,0 +1,20 @@
+package xxgamehelper.framework.model;
+
+/***
+ * Some common browser UA strings.
+ * @author LongFangzhou
+ */
+public class UserAgent {
+	public static String IOS = "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1C28 Safari/419.3";
+	public static String IOS2 = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F136 Safari/525.20";
+	public static String IOS3 = "Mozilla/5.0 (iPhone; CPU iPhone OS 3_1_3 like Mac OS X; ja-jp) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7E18 Safari/528.16";
+	public static String IOS4 = "Mozilla/5.0 (iPhone; CPU iPhone OS 4_3_5 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8L1";
+	public static String IOS5 = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9A405 Safari/7534.48.3";
+	public static String IOS6 = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Mobile/10B142";
+	public static String IOS7 = "mozilla/5.0 (iPhone; CPU iPhone os 7_0_2 like mac os x) AppleWebKit/537.51.1 (KHTML, like Gecko) version/7.0 Mobile/11a501 Safari/9537.53";
+	
+	public static String ANDOIRD1_6 = "Mozilla/5.0 (Linux; U; Android 1.5; ja-jp; GDDJ-09 Build/CDB56) AppleWebKit/528.5+ (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1";
+	public static String ANDOIRD2_X = "Mozilla/5.0 (Linux; U; Android 2.3.5; ja-jp; T-01D Build/F0001) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+	public static String ANDOIRD3_X = "Mozilla/5.0 (Linux; U; Android 3.2.1; ja-jp; Transformer TF101 Build/HTK75) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13";
+	public static String ANDOIRD4_X = "Mozilla/5.0 (Linux; U; Android 4.1.1; ja-jp; Galaxy Nexus Build/JRO03H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
+}
