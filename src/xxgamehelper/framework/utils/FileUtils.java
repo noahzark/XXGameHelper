79839d11d328f2db687b8e01c56e81b82f8bc8c2
noahzark <noahzark@vip.qq.com>
2014-02-11 21:36:41 +0800

    Move saveRspToFile method to FileUtils
    It's a static method operating on files!

--- src/xxgamehelper/framework/utils/FileUtils.java
+++ src/xxgamehelper/framework/utils/FileUtils.java
@@ -1,7 +1,18 @@
 package xxgamehelper.framework.utils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import xxgamehelper.framework.model.Core;
 
 /***
  * Some tools to operate on files.
@@ -38,6 +49,38 @@ public class FileUtils {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
+		}
+		return true;
+	}
+	
+	/***
+	 * The method to save a response to the file
+	 * @param rsp The http response
+	 * @param fileName The target file
+	 * @throws IOException 
+	 */
+	public static boolean saveRspToFile(HttpResponse rsp, String fileName) throws IOException{
+		HttpEntity entity = rsp.getEntity();
+		if (entity != null) {
+			Header[] coding = rsp.getHeaders("Content-Encoding");
+			InputStream is = null;
+			try {
+				if (coding.length>0)
+					is = new GZIPInputStream(entity.getContent());
+				else
+					is = entity.getContent();
+				FileOutputStream fos = new FileOutputStream(new File(fileName));
+				byte[] b = new byte[Core.BUFFERSIZE];
+				int len = 0;
+				while((len=is.read(b))!=-1)
+					fos.write(b,0,len);
+				fos.close();
+			} catch (IllegalStateException | IOException e) {
+//				this.messenger.showError(e);
+				return false;
+			} finally {
+				EntityUtils.consume(entity);
+			}
 		}
 		return true;
 	}
