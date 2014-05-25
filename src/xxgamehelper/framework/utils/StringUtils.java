98c561b2d95c79684e6f1b3f3d8c91a15e0d22bc
noahzark <noahzark@vip.qq.com>
2014-05-25 14:34:23 +0800

    Fix the StringUtils parameter
    Add filePath parameter to work with other functions

--- src/xxgamehelper/framework/utils/StringUtils.java
+++ src/xxgamehelper/framework/utils/StringUtils.java
@@ -17,37 +17,40 @@ public class StringUtils {
 	/***
 	 * Check if a key string is in a file(Default use UTF-8 char set to open it).
 	 * @param key The key String
-	 * @param filePath The name of target file
+	 * @param workPath The directory path end with "/"
+	 * @param fileName The name of target file
 	 * @return Null if the key is not found,
 	 * else return the line where the string first occurred
 	 */
-	public static String searchForString(String key, String filePath){
-		return StringUtils.searchString(key, filePath, "UTF-8");
+	public static String searchForString(String key, String workPath, String fileName){
+		return StringUtils.searchForString(key, workPath, fileName, "UTF-8");
 	}
 	
 	/***
 	 * Get all lines which contains the key string in a file(Default use UTF-8 char set to open it).
 	 * @param key The key string
-	 * @param filePath The name of target file
+	 * @param workPath The directory path end with "/"
+	 * @param fileName The name of target file
 	 * @param charsetName  The char set used to open the file
 	 * @return A string array
 	 */
-	public static String[] searchAllString(String key, String filePath) {
-		return StringUtils.searchAllString(key, filePath, "UTF-8");
+	public static String[] searchAllString(String key, String workPath, String fileName) {
+		return StringUtils.searchAllString(key, workPath, fileName, "UTF-8");
 	}
 	
 	/***
 	 * Check if a key string is in a file.
 	 * @param key The key String
-	 * @param filePath The name of target file
+	 * @param workPath The directory path end with "/"
+	 * @param fileName The name of target file
 	 * @param charsetName The char set used to open the file
 	 * @return Null if the key is not found,
 	 * else return the line where the string first occurred
 	 */
-	public static String searchString(String key, String filePath, String charsetName) {
+	public static String searchForString(String key, String workPath, String fileName, String charsetName) {
 		try {
 			InputStreamReader isr = new InputStreamReader(
-					new FileInputStream(filePath), charsetName);
+					new FileInputStream(workPath+fileName), charsetName);
 			BufferedReader br = new BufferedReader(isr);
 			String s;
 			while ((s = br.readLine())!=null){
@@ -68,15 +71,16 @@ public class StringUtils {
 	/***
 	 * Get all lines which contains the key string in a file.
 	 * @param key The key string
+	 * @param workPath The directory path end with "/"
 	 * @param filePath The name of target file
 	 * @param charsetName  The char set used to open the file
 	 * @return A string array
 	 */
-	public static String[] searchAllString(String key, String filePath, String charsetName) {
+	public static String[] searchAllString(String key, String workPath, String filePath, String charsetName) {
 		ArrayList<String> list = new ArrayList<String>();
 		try {
 			InputStreamReader isr = new InputStreamReader(
-					new FileInputStream(filePath), charsetName);
+					new FileInputStream(workPath+filePath), charsetName);
 			BufferedReader br = new BufferedReader(isr);
 			String s;
 			while ((s = br.readLine())!=null){
