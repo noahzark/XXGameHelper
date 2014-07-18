806df4442dd273142bfa14e46c28ec9b8c4ee761
noahzark <noahzark@vip.qq.com>
2014-07-19 00:21:22 +0800

    Modified GameConfigDetail
    Add showConfig method

--- src/xxgamehelper/framework/model/configuration/GameConfigDetail.java
+++ src/xxgamehelper/framework/model/configuration/GameConfigDetail.java
@@ -1,6 +1,7 @@
 package xxgamehelper.framework.model.configuration;
 
 import java.util.HashMap;
+import java.util.Set;
 
 /***
  * The class to store game configurations of specific type.
@@ -9,18 +10,26 @@ import java.util.HashMap;
  */
 public final class GameConfigDetail<ValueType> {
 	
-	private final HashMap<String, ValueType> map
+	private final HashMap<String, ValueType> configMap
 	= new HashMap<String, ValueType>();
 	
 	public ValueType getConfig(String key) {
-		return map.get(key);
+		return configMap.get(key);
 	}
 	
 	public boolean setConfig(String key, ValueType value) {
-		if (map.containsKey(key))
+		if (configMap.containsKey(key))
 			return false;
-		map.put(key, value);
+		configMap.put(key, value);
 		return true;
+	}
+	
+	public String showConfig() {
+		StringBuffer sb = new StringBuffer();
+		Set<String> keySet = configMap.keySet();
+		for (String key : keySet)
+			sb.append("Key:"+key+"\tValue:"+configMap.get(key)+"\n");
+		return sb.toString();
 	}
 
 }
