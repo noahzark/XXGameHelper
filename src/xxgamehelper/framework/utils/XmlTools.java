da77bba47572421fdf399d7a8165d44a20eaf733
noahzark <noahzark@vip.qq.com>
2014-07-18 23:14:47 +0800

    Remove unused import in XmlTools

--- src/xxgamehelper/framework/utils/XmlTools.java
+++ src/xxgamehelper/framework/utils/XmlTools.java
@@ -15,7 +15,6 @@ import javax.xml.validation.Validator;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
