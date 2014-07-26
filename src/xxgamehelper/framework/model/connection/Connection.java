9171775ed53eaeef9585374ee6af5542e0855ee6
noahzark <noahzark@vip.qq.com>
2014-07-27 02:07:26 +0800

    Connection now needs to implement NetOperationInterface

--- src/xxgamehelper/framework/model/connection/Connection.java
+++ src/xxgamehelper/framework/model/connection/Connection.java
@@ -1,10 +1,12 @@
 package xxgamehelper.framework.model.connection;
 
+import xxgamehelper.framework.model.NetOperationInterface;
+
 /***
  * Programs should use connection class to login to the server
  * and generate the web client.
  * @author LongFangzhou
  */
-public abstract class Connection extends ConnectionData implements ConnectionInterface{
+public abstract class Connection extends ConnectionData implements ConnectionInterface, NetOperationInterface{
 
 }
