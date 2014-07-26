f25ceb1835fc88e6b3b047c29e848df4f197e329
noahzark <noahzark@vip.qq.com>
2014-07-27 01:16:49 +0800

    Make CoreInterface more clearly
    Remove unneccessary modifiers.

--- src/xxgamehelper/framework/model/core/CoreInterface.java
+++ src/xxgamehelper/framework/model/core/CoreInterface.java
@@ -4,27 +4,19 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.http.NameValuePair;
-import org.apache.http.client.methods.HttpRequestBase;
 
 /***
  * The core class should be runnable to improve stability and achieve multi-thread.
  * @author LongFangzhou
  */
-public abstract interface CoreInterface extends Runnable {
+public interface CoreInterface extends Runnable {
 
 	/***
 	 * Do some check or other operations before sending a request.
 	 * @param remoteAddress
 	 * @return True if checks passed, otherwise false
 	 */
-	abstract boolean preRequest(String remoteAddress, String fileName);
-	
-	/***
-	 * Inject some request headers to a request
-	 * @param request The target request
-	 * @param headers The headers map
-	 */
-	abstract void injectHeaders(HttpRequestBase request, Map<String, String> headers);
+	boolean preRequest(String remoteAddress, String fileName);
 	
 	/***
 	 * Use GET method to obtain web content.
@@ -32,7 +24,7 @@ public abstract interface CoreInterface extends Runnable {
 	 * @param fileName A file to save those content
 	 * @return If the operation succeed, return true. Otherwise false.
 	 */
-	abstract boolean doGet(String remoteAddress, String fileName);
+	boolean doGet(String remoteAddress, String fileName);
 	
 	/***
 	 * Use GET method to obtain web content with some request headers.
@@ -41,7 +33,7 @@ public abstract interface CoreInterface extends Runnable {
 	 * @param fileName A file to save those content
 	 * @return If the operation succeed, return true. Otherwise false.
 	 */
-	abstract boolean doGet(String remoteAddress, Map<String, String> headers
+	boolean doGet(String remoteAddress, Map<String, String> headers
 			, String fileName);
 	
 	/***
@@ -51,7 +43,7 @@ public abstract interface CoreInterface extends Runnable {
 	 * @param fileName A file to save those content
 	 * @return If the operation succeed, return true. Otherwise false.
 	 */
-	abstract boolean doPost(String remoteAddress, List<NameValuePair> formParams,
+	boolean doPost(String remoteAddress, List<NameValuePair> formParams,
 			String fileName);
 	
 	/***
@@ -62,29 +54,29 @@ public abstract interface CoreInterface extends Runnable {
 	 * @param fileName A file to save those content
 	 * @return If the operation succeed, return true. Otherwise false.
 	 */
-	abstract boolean doPost(String remoteAddress, List<NameValuePair> formParams,
+	boolean doPost(String remoteAddress, List<NameValuePair> formParams,
 			Map<String, String> headers, String fileName);
 	
 	/***
 	 * The method to clean temporary work files.
 	 * You can use utils.FileUtils.deleteFiles() for help.
 	 */
-	abstract void cleanFiles();
+	void cleanFiles();
 
 	/***
 	 * The method to initialize the game runtime environment.
 	 */
-	abstract void initGame();
+	void initGame();
 	
 	/***
 	 * The method to run the game.
 	 */
-	abstract void runGame();
+	void runGame();
 	
 	/***
 	 * Do some operations when the game is finish.
 	 */
-	abstract void postGame();
+	void postGame();
 	
 	
 	/***
