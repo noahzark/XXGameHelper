ed5edb0d1f9d50e88101fd6e07525755070ed26f
noahzark <noahzark@vip.qq.com>
2014-07-27 02:07:47 +0800

    Realise new methods in DefaultConnection

--- src/xxgamehelper/framework/model/connection/DefaultConnection.java
+++ src/xxgamehelper/framework/model/connection/DefaultConnection.java
@@ -1,6 +1,7 @@
 package xxgamehelper.framework.model.connection;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -9,6 +10,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnRoutePNames;
 
+import xxgamehelper.framework.control.OutputInterface;
 import xxgamehelper.framework.control.messenger.Messenger;
 import xxgamehelper.framework.model.client.WebClient;
 import xxgamehelper.framework.utils.CoreEntityUtils;
@@ -16,9 +18,11 @@ import xxgamehelper.framework.utils.CoreEntityUtils;
 /***
  * A sample of Connection's implement to provide basic/default functions.
  * @author LongFangzhou
- * @version 0.2
+ * @version 0.3
  */
 public abstract class DefaultConnection extends Connection {
+	
+	private OutputInterface out;
 	
 	public void useProxy(String address, int port) {
 		HttpHost proxy = new HttpHost(address, port);
@@ -32,6 +36,7 @@ public abstract class DefaultConnection extends Connection {
 	public DefaultConnection(Messenger messenger, WebClient webclient){
 		this.messenger = messenger;
 		this.webclient = webclient;
+		this.out = messenger;
 	}
 	
 	public boolean loadHelperConfig() {
@@ -70,6 +75,42 @@ public abstract class DefaultConnection extends Connection {
 		this.server = new HttpHost(address, 80, "http");
 	}
 	
+	public boolean preRequest(String remoteAddress, String fileName) {
+		if (this.server == null){
+			out.showError("Server is null");
+			return false;
+		}
+		if (this.webclient == null) {
+			out.showError("Webclient is null", "Maybe developer forgot to save connection");
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean doGet(String remoteAddress, String fileName) {
+		return this.doGet(remoteAddress, null, fileName);
+	}
+	
+	public boolean doGet(String remoteAddress, Map<String, String> headers, String fileName) {
+		if (this.preRequest(remoteAddress, fileName)){
+			return webclient.doGet(server, remoteAddress, headers, fileName);
+		}
+		return false;
+	}
+	
+	public boolean doPost(String remoteAddress, List<NameValuePair> formParams,
+			String fileName){
+		return this.doPost(remoteAddress, formParams, null, fileName);
+	}
+	
+	public boolean doPost(String remoteAddress, List<NameValuePair> formParams,
+			Map<String, String> headers, String fileName){
+		if (this.preRequest(remoteAddress, fileName)){
+			return webclient.doPost(server, remoteAddress, formParams, headers, fileName);
+		}
+		return false;
+	}
+	
 	/***
 	 * Use POST method to obtain web content.
 	 * @param host Target host
@@ -78,6 +119,7 @@ public abstract class DefaultConnection extends Connection {
 	 * @param fileName A file to save those content.
 	 * @return If the operation succeed, return true. Otherwise false.
 	 */
+	@Deprecated
 	public boolean doPost(HttpHost host, HttpPost req,
 			List<NameValuePair> formParams, String fileName) {
 		HttpEntity entity = CoreEntityUtils.generateEntity(formParams);
@@ -92,6 +134,7 @@ public abstract class DefaultConnection extends Connection {
 	 * @param fileName A file to save those content.
 	 * @return If the operation succeed, return true. Otherwise false.
 	 */
+	@Deprecated
 	public boolean doGet(HttpHost host, HttpGet req, String fileName) {
 		return this.webclient.saveRequestToFile(host, req, fileName);
 	}
