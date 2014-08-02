d7ef6466b9d5c6e6d3967adf9c1faa79520bab47
noahzark <noahzark@vip.qq.com>
2014-08-02 17:03:52 +0800

    Modified HelperLauncher
    Make the launchCore operations an individual method,
    Add rest and restart differently in different situations in
    launchWithChecker method

--- src/xxgamehelper/framework/model/HelperLauncher.java
+++ src/xxgamehelper/framework/model/HelperLauncher.java
@@ -17,10 +17,15 @@ public class HelperLauncher {
 	
 	static Logger logger = Logger.getLogger(HelperLauncher.class);
 	
+	private static void launchCore(Messenger messenger) {
+		Core core = messenger.getHelperFactory().buildCore(messenger);
+		messenger.setHelperThread(new Thread(core));
+		messenger.startHelper();
+	}
+	
 	/***
 	 * Launch the helper with a messenger and a helper factory.
 	 * @param messenger The helper messenger
-	 * @param factory The helper factory
 	 * @return The helper thread
 	 */
 	public static void launchWithoutCheck(Messenger messenger) {
@@ -31,9 +36,7 @@ public class HelperLauncher {
 		Connection tscon = messenger.getHelperFactory().buildConnection(messenger);
 		logger.info("Try to lanch a helper.");
 		if (tscon.connect()) {
-				Core core = messenger.getHelperFactory().buildCore(messenger);
-				messenger.setHelperThread(new Thread(core));
-				messenger.startHelper();
+				HelperLauncher.launchCore(messenger);
 				logger.info("Launch succeeded.");
 			}
 	}
@@ -48,27 +51,45 @@ public class HelperLauncher {
 	public static void launchWithCheker(Messenger messenger, int checkInterval) {
 		logger.info("Try to launch helper with a checker per " + checkInterval + " seconds.");
 		messenger.releaseHelperThread();
+		HelperLauncher.launchWithoutCheck(messenger);
+		
+		long lstLaunchTime = (new Date()).getTime();
+		int restartCount = 0;
+		int waitInterval = checkInterval;
 		while (true){
-			HelperLauncher.launchWithoutCheck(messenger);
 			long verifyToken = 0L;
 			try {
 				do {
 					messenger.println("The helper thread is alive.");
 					verifyToken = messenger.getVerifyToken();// Remember the verify token
-					Thread.sleep(checkInterval*1000);// Take a break to wait core thread update token
+					Thread.sleep(waitInterval*1000);// Take a break to wait core thread update token
 				} while(messenger.isHelperAlive() && verifyToken != messenger.getVerifyToken());
-				messenger.println((new Date()) + ": The helper thread is dead.");
+				long nowTime = (new Date()).getTime();
+				messenger.println(nowTime + ": The helper thread is dead.");
 				//If a verify token equals zero, it aborts normally and
 				//don't need to dump work files.
-				if (messenger.getVerifyToken()!=0L)
+				if (messenger.getVerifyToken()!=0L) {
 					messenger.dumpWorkFiles();
-				messenger.println("Try to restart the helper thread");
+					if (nowTime<=lstLaunchTime + 5*waitInterval*1000)
+						restartCount++;
+				}
 			} catch (InterruptedException e) {
 				messenger.showError(e);
 			} finally {
 				//TODO Do some clean jobs here
 				messenger.releaseHelperThread();
 			}
+			if (restartCount<10) {
+				messenger.println("Try to restart the helper core thread.");
+				HelperLauncher.launchCore(messenger);
+				waitInterval = checkInterval;
+			} else {
+				messenger.println("Try to restart the helper thread completely.");
+				HelperLauncher.launchWithoutCheck(messenger);
+				waitInterval = checkInterval * 5;
+				restartCount = 0;
+			}
+			lstLaunchTime = (new Date()).getTime();
 		}	
 	}
 
